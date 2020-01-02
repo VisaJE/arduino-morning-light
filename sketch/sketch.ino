fd415f3482e1eb9b453678fe68356951bbfd58d1
VisaJE <eemil@live.com>
2020-01-02 23:00:56 +0000

    Switch version

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,7 +2,7 @@
 #include <avr/wdt.h>
 
 constexpr int CTRL = 6;
-constexpr int PIEZOPIN = A0;
+constexpr int SWITCHPIN = 7;
 
 constexpr int LIGHTON = 65;
 constexpr int CENTERED = 85;
@@ -16,135 +16,53 @@ constexpr int maxAng = 175;
 #define time_t unsigned long
 
 
-// Average array definition
-
-struct Node
-{
-    float value;
-    Node* newer;
-};
-
-class AverageArray
-{
-        int leng;
-        int realleng;
-
-        Node* first;
-        Node* last;
-    public:
-        float total;
-        float average() {return total / realleng;}
-
-        int Length() {return realleng;}
-
-        AverageArray(int len = 0, float total=0)
-        {
-            realleng = 0;
-            leng = len;
-            if (len <= 0)
-            {
-                leng = 0;
-            }
-            this->total = total;
-
-            last = NULL;
-            first = NULL;
-        }
-
-        ~AverageArray()
-        {
-            Node* rem = last;
-            while (rem != NULL)
-            {
-                last = rem->newer;
-                delete rem;
-                rem = last;
-            }
-        }
-
-        void updateArray(float value)
-        {
-            if (leng == 0) return;
-            if (realleng < leng)
-            {
-                Node* nod = new Node;
-                nod->value = value;
-                if (first != NULL)
-                    first->newer = nod;
-                first = nod;
-            }
-
-            if (last != NULL && leng == realleng)
-            {
-                total -= last->value;
-
-                auto slast = last->newer;
-
-                last->value = value; // Reusing last Node
-                last->newer = NULL;
-                first->newer = last;
-                first = last;
-                last = slast;
-            }
-            else if (last == NULL)
-            {
-                last = first;
-            }
-
-            total += first->value;
-            if (realleng < leng)
-                    realleng += 1;
-        }
-};
-
-
 class Time
 {
-private:
-  time_t ms;
-  time_t mins;
-  time_t hs;
+  private:
+    time_t ms;
+    time_t mins;
+    time_t hs;
 
-public:
-  time_t get_ms()
-  {
-    normalize();
-    return ms;
-  }
-  time_t get_mins()
-  {
-    normalize();
-    return mins;
-  }
-  time_t get_hs()
-  {
-    normalize();
-    return hs;
-  }
-
-  void normalize()
-  {
-    if (ms >= 60000)
+  public:
+    time_t get_ms()
     {
-      time_t delta = ms % 60000;
-      mins += (ms - delta) / 60000;
-
-      time_t deltam = mins % 60;
-      hs += (mins - deltam) / 60;
-
-      ms = delta;
-      mins = deltam;
+      normalize();
+      return ms;
     }
-  }
+    time_t get_mins()
+    {
+      normalize();
+      return mins;
+    }
+    time_t get_hs()
+    {
+      normalize();
+      return hs;
+    }
+
+    void normalize()
+    {
+      if (ms >= 60000)
+      {
+        time_t delta = ms % 60000;
+        mins += (ms - delta) / 60000;
+
+        time_t deltam = mins % 60;
+        hs += (mins - deltam) / 60;
+
+        ms = delta;
+        mins = deltam;
+      }
+    }
 
   Time(time_t millisecs=0,time_t minutes=0, time_t hours=0):
-    ms(millisecs), mins(minutes), hs(hours)
+      ms(millisecs), mins(minutes), hs(hours)
   {
     normalize();
   }
   ~Time() {}
 
-  void addMs(time_t millisecs)
+    void addMs(time_t millisecs)
   {
     ms += millisecs;
     //normalize(); // Stupid implementation
@@ -159,35 +77,31 @@ public:
   {
     return Time(t1.ms + t2.ms, t1.mins + t2.mins, t1.hs + t2.hs);
   }
-  friend Time operator- (const Time& t1, const Time& t2)
-  {
-    return Time(t1.asMs() - t2.asMs());
-  }
   friend Time operator* (const int& sc, const Time& t2)
   {
     return Time(sc * t2.ms, sc * t2.mins, sc * t2.hs);
   }
 
-  void print()
-  {
-    normalize();
-    Serial.print("Time ");
-    Serial.print(hs);
-    Serial.print("h ");
-    Serial.print(mins);
-    Serial.print("min ");
-    Serial.print(ms/1000);
-    Serial.println("s");
-  }
-// Only one minute accuracy
-  friend bool operator== (const Time& t1, const Time& t2)
-  {
-    return t1.hs == t2.hs && t1.mins == t2.mins;
-  }
-  friend bool operator!= (const Time& t1, const Time& t2)
-  {
-    return !(t1 == t2);
-  }
+    void print()
+    {
+      normalize();
+      Serial.print("Time ");
+      Serial.print(hs);
+      Serial.print("h ");
+      Serial.print(mins);
+      Serial.print("min ");
+      Serial.print(ms/1000);
+      Serial.println("s");
+    }
+    // Only one minute accuracy
+    friend bool operator== (const Time& t1, const Time& t2)
+    {
+      return t1.hs == t2.hs && t1.mins == t2.mins;
+    }
+    friend bool operator!= (const Time& t1, const Time& t2)
+    {
+      return !(t1 == t2);
+    }
 
   friend bool operator> (const Time& t1, const Time& t2)
   {
@@ -207,13 +121,10 @@ const Time wakeDeltaDelta = Time(0, 15, 0);
 const Time offWait = Time(10000);
 
 Servo myservo;  // create servo object to control a servo
-bool pressDir = true; // Stores the sign of the voltage delta.
 bool pressed = false;
-bool firstSet = false; // Set the initial press dir
 
 bool offDone;
 bool onDone;
-AverageArray dbuffer;
 
 Time lightT;
 Time pressT;
@@ -227,10 +138,10 @@ void moveservo(int pos, int del=500)
     myservo.attach(CTRL);
     if (pos > minAng)
     {
-        if (pos < maxAng)
-          myservo.write(pos);
-        else
-          myservo.write(maxAng);
+      if (pos < maxAng)
+        myservo.write(pos);
+      else
+        myservo.write(maxAng);
     }
     else
       myservo.write(minAng);
@@ -249,11 +160,7 @@ void setup() {
   offDone = true;
   onDone = true;
 
-  dbuffer = AverageArray(5);
-  int sensorValue = analogRead(PIEZOPIN);
-  float voltage = sensorValue * (5.0 / 1023.0);
-  dbuffer.updateArray(voltage); // Keep track of long time average
-
+  pinMode(SWITCHPIN, INPUT);
   auto timenow = Time(millis());
 
 }
@@ -264,54 +171,39 @@ constexpr float sensitivity= 0.6;
 void loop() {
   auto timenow = Time(millis());
 
-  int sensorValue = analogRead(PIEZOPIN);
-  float voltage = sensorValue * (5.0 / 1023.0);
+  auto switchPos = digitalRead(SWITCHPIN) == HIGH;
 
-  dbuffer.updateArray(voltage); // Keep track of long time average
-  float dif = voltage - dbuffer.average();
-  float chg = abs(dif);
-
-  if (chg > sensitivity)
+  if (pressed != switchPos);
   {
-    bool dir = (bool)(dif/chg + 1);
 
-    if (!firstSet)
+    if (switchPos)
     {
-      pressDir = !dir;
-      firstSet = true;
+      pressT = Time(millis());
+      pressed = true;
     }
-
-    if (pressDir != dir)
+    else
     {
-      if (!pressed)
-      {
-        pressT = Time(millis());
-        pressed = true;
-      }
-      else
-      {
-        int pressLength = (int)((timenow.asMs() - pressT.asMs()) / 1000);
-        lightT = timenow + wakeDelta + pressLength * wakeDeltaDelta;
-        offT = timenow + offWait;
+      int pressLength = (int)((timenow.asMs() - pressT.asMs()) / 1000);
+      lightT = timenow + wakeDelta + pressLength * wakeDeltaDelta;
+      offT = timenow + offWait;
 
-        //Serial.print("Currently ");
-        //timenow.print();
-        //Serial.print("Light on at ");
-        //lightT.print();
-        //Serial.print("presslength was ");
-        //Serial.println(pressLength);
+      //Serial.print("Currently ");
+      //timenow.print();
+      //Serial.print("Light on at ");
+      //lightT.print();
+      //Serial.print("presslength was ");
+      //Serial.println(pressLength);
 
-        for (int i = 0; i < pressLength + 1; i++)
-        {
-          moveservo(NOTIFY, 100);
-          moveservo(CENTERED, 250);
-        }
-        offDone = false;
-        onDone = false;
-        pressed = false;
+      for (int i = 0; i < pressLength + 1; i++)
+      {
+        moveservo(NOTIFY, 100);
+        moveservo(CENTERED, 250);
       }
+      offDone = false;
+      onDone = false;
+      pressed = false;
     }
-    pressDir = dir;
+    pressed = switchPos;
   }
 
 
@@ -331,16 +223,6 @@ void loop() {
 
   wdt_reset();
 
-  //Serial.print(voltage);
-  //Serial.print(",");
-  //Serial.print(dbuffer.average());
-  //Serial.print(",");
-
-  //Serial.print(0.9);
-  //Serial.print(",");
-  //Serial.print(-0.9);
-  //Serial.print(",");
-  //Serial.println(voltage - dbuffer.average());
   //Serial.print(pressed);
   delay(250);
 }
